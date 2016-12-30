a524a9a0fe7c1a50bb52817fc248304704fffb25
Michael <michael.chung@metacloud.ca>
2016-12-30 15:33:53 -0500

    Initial Commit

---
+++ Overkill/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Overkill
+//
+//  Created by Michael Chung on 12/30/16.
+//  Copyright © 2016 angelica.ramos. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
